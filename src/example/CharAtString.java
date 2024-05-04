8a3c6986793dce4326c9c3f0874b6aa27e6d75c9
MochJuang <mochjuangpp@gmail.com>
2024-05-04 19:41:51 +0700

    add example

---
+++ src/example/CharAtString.java
@@ -0,0 +1,9 @@
+package example;
+
+public class CharAtString {
+    
+    public static void main(String []args){
+        String name = "Mangcoding";
+        System.out.println("The result is the letters: " + name.charAt(4));
+    }
+}
---
+++ src/example/LengthString.java
@@ -0,0 +1,8 @@
+package example;
+
+public class LengthString {
+    public static void main(String[] args) throws Exception{
+        String name = "Mangcoding";
+        System.out.println("the result is: " + name.length());
+    }
+}
