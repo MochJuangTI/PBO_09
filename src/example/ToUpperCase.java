b0b56901be7631ff9b533eebcb347573ca4a7c20
MochJuang <mochjuangpp@gmail.com>
2024-05-04 19:47:34 +0700

    add example

--- src/example/ToUpperCase.java
+++ src/example/ToUpperCase.java
@@ -1,1 +1,11 @@
 package example;
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+        String stringOri = "Walcome to Mangcodig";
+        String stringUpper = stringOri.toUpperCase();
+        System.out.println("Before using the toUpperCase method: " + stringOri);
+        System.out.println("After using the toUpperCase method: " + stringUpper);
+         
+    }
+}
